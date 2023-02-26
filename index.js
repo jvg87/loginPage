81291ef180efb05ff349c8d82ea414566fb11a65
João Victor Garcia <121620145+jvg87@users.noreply.github.com>
2023-02-26 09:16:23 -0300

    terminando o css do login

---
+++ index.js
@@ -0,0 +1,5 @@
+
+document.querySelector('.mobileMenu').addEventListener('click', () =>{
+    document.querySelector('.navigation').classList.toggle('showMenu')
+    document.querySelector('.mobileMenu').classList.toggle('showMenu')
+})
