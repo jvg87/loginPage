16467a91d766c9acbbda50bc703ba8028299c9ca
João Victor Garcia <121620145+jvg87@users.noreply.github.com>
2023-02-26 10:55:33 -0300

    finalizando estilização

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ document.querySelector('.mobileMenu').addEventListener('click', () =>{
     document.querySelector('.navigation').classList.toggle('showMenu')
     document.querySelector('.mobileMenu').classList.toggle('showMenu')
 })
+
+document.querySelector('.registerLink').addEventListener('click', () => {
+    document.querySelector('#containerForm').classList.add('active')
+})
+
+document.querySelector('.loginLink').addEventListener('click', () => {
+    document.querySelector('#containerForm').classList.remove('active')
+})
