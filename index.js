c83979771481efe3feac5d089d61bd6158a972c8
João Victor Garcia <121620145+jvg87@users.noreply.github.com>
2023-02-26 13:51:45 -0300

    finalizado visual site

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-
+// NavBar
 document.querySelector('.mobileMenu').addEventListener('click', () =>{
     document.querySelector('.navigation').classList.toggle('showMenu')
     document.querySelector('.mobileMenu').classList.toggle('showMenu')
 })
 
+// Switch Login/Register
 document.querySelector('.registerLink').addEventListener('click', () => {
     document.querySelector('#containerForm').classList.add('active')
 })
@@ -11,3 +12,22 @@ document.querySelector('.registerLink').addEventListener('click', () => {
 document.querySelector('.loginLink').addEventListener('click', () => {
     document.querySelector('#containerForm').classList.remove('active')
 })
+
+// Button Login
+
+document.querySelector('header input').addEventListener('click', () =>{
+    document.querySelector('#conteinerLoginRegister').classList.add('activePopup')
+    document.querySelector('.background').classList.add('activePopup')
+})
+
+document.querySelector('.iconClose').addEventListener('click', () =>{
+    document.querySelector('#conteinerLoginRegister').classList.remove('activePopup')
+    document.querySelector('.background').classList.remove('activePopup')
+})
+
+// View Password
+
+document.querySelector('#fa-eye-login').addEventListener('click' () => {
+    
+})
+
