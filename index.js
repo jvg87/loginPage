2e2ad0837d9aa0ea10df436a398d39ade1c0d097
João Victor Garcia <121620145+jvg87@users.noreply.github.com>
2023-02-26 22:39:13 -0300

    começando as validações

--- index.js
+++ index.js
@@ -1,33 +1,119 @@
+const mobileMenu = document.querySelector('.mobileMenu')
+const navigation = document.querySelector('.navigation')
+
+const registerLink = document.querySelector('.registerLink')
+const containerForm = document.querySelector('#containerForm')
+
+const loginLink = document.querySelector('.loginLink')
+const headerInput = document.querySelector('header input')
+
+const conteinerLoginRegister = document.querySelector('#conteinerLoginRegister')
+const background = document.querySelector('.background')
+const iconClose = document.querySelector('.iconClose')
+
+const faEyeLogin = document.querySelector('#fa-eye-login')
+const passwordLogin = document.querySelector('#passwordLogin')
+const faEyeRegister = document.querySelector('#fa-eye-register')
+const passwordRegister = document.querySelector('#passwordRegister')
+const faEyeConfirm = document.querySelector('#fa-eye-confirm')
+const passwordConfirm = document.querySelector('#passwordConfirm')
+
+const form = document.querySelector('form')
+const usernameRegister = document.querySelector('#usernameRegister')
+const labelUsernameRegister = document.querySelector('#labelUsernameRegister')
+const emailRegister = document.querySelector('#emailRegister')
+const labelEmailRegister = document.querySelector('#labelEmailRegister')
+const labelPasswordRegister = document.querySelector('#labelPasswordRegister')
+const labelPasswordConfirm = document.querySelector('#labelPasswordConfirm')
+
+
+
+
 // NavBar
-document.querySelector('.mobileMenu').addEventListener('click', () =>{
-    document.querySelector('.navigation').classList.toggle('showMenu')
-    document.querySelector('.mobileMenu').classList.toggle('showMenu')
+mobileMenu.addEventListener('click', () =>{
+    navigation.classList.toggle('showMenu')
+    mobileMenu.classList.toggle('showMenu')
 })
 
 // Switch Login/Register
-document.querySelector('.registerLink').addEventListener('click', () => {
-    document.querySelector('#containerForm').classList.add('active')
+registerLink.addEventListener('click', () => {
+    containerForm.classList.add('active')
 })
 
-document.querySelector('.loginLink').addEventListener('click', () => {
-    document.querySelector('#containerForm').classList.remove('active')
+loginLink.addEventListener('click', () => {
+    containerForm.classList.remove('active')
 })
 
 // Button Login
 
-document.querySelector('header input').addEventListener('click', () =>{
-    document.querySelector('#conteinerLoginRegister').classList.add('activePopup')
-    document.querySelector('.background').classList.add('activePopup')
+headerInput.addEventListener('click', () =>{
+    conteinerLoginRegister.classList.add('activePopup')
+    background.classList.add('activePopup')
 })
 
-document.querySelector('.iconClose').addEventListener('click', () =>{
-    document.querySelector('#conteinerLoginRegister').classList.remove('activePopup')
-    document.querySelector('.background').classList.remove('activePopup')
+iconClose.addEventListener('click', () =>{
+    conteinerLoginRegister.classList.remove('activePopup')
+    background.classList.remove('activePopup')
 })
 
 // View Password
 
-document.querySelector('#fa-eye-login').addEventListener('click' () => {
-    
+faEyeLogin.addEventListener('click', () => {
+    if (passwordLogin.type === 'password'){
+        passwordLogin.setAttribute('type', 'text')
+        faEyeLogin.className = 'fa-solid fa-eye-slash'
+
+    } else {
+        passwordLogin.setAttribute('type', 'password')
+        faEyeLogin.className = 'fa-solid fa-eye'
+    } 
 })
+
+faEyeRegister.addEventListener('click', () => {
+    if (passwordRegister.type === 'password'){
+        passwordRegister.setAttribute('type', 'text')
+        faEyeRegister.className = 'fa-solid fa-eye-slash'
+
+    } else {
+        passwordRegister.setAttribute('type', 'password')
+        faEyeRegister.className = 'fa-solid fa-eye'
+    } 
+})
+
+faEyeConfirm.addEventListener('click', () => {
+    if (passwordConfirm.type === 'password'){
+        passwordConfirm.setAttribute('type', 'text')
+        faEyeConfirm.className = 'fa-solid fa-eye-slash'
+    } else {
+        passwordConfirm.setAttribute('type', 'password')
+        faEyeConfirm.className = 'fa-solid fa-eye'
+    } 
+})
+
+// Register Validation
+
+// form.addEventListener('submit', (ev) => {
+//     ev.preventDefault()
+// })
+
+usernameRegister.addEventListener('keyup', () => {
+    if(usernameRegister.value === ''){
+        usernameRegister.className = 'error'
+        //pegar icon
+        labelUsernameRegister.setAttribute('style', 'color:red')
+    } else if(usernameRegister.value.length <= 3) {
+        usernameRegister.className = 'error'
+        //pegar icon
+        labelUsernameRegister.setAttribute('style', 'color:red')
+    } else {
+        usernameRegister.className = 'sucess'
+        //pegar icon    
+        labelUsernameRegister.setAttribute('style', 'color:green')
+    }
+})
+
+// function checkInput(){
+    
+// }
+
 
